308fa3e9abb5562437cbc77c557f2310d3d178bc
Pawan Sarngal <pawansarngal@gmail.com>
2024-05-04 16:35:52 +0530

    corrected-syntax

--- main.tf
+++ main.tf
@@ -34,10 +34,11 @@ module "blog_sg" {
   name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
+
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all_all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
