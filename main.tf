ddeedb350f07d080d6648b6e2fd38c5d09716594
Pawan Sarngal <pawansarngal@gmail.com>
2024-05-05 20:29:49 +0530

    added alb

--- main.tf
+++ main.tf
@@ -24,15 +24,15 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
   # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   # enable_nat_gateway = true
   # enable_vpn_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -50,17 +50,63 @@ resource "aws_instance" "blog" {
   }
 }
 
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name               = "blog-alb"
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  # access_logs = {
+  #   bucket = "my-alb-logs"
+  # }
+
+  listeners = {
+    http = {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+
+  }
+
+  target_groups = [
+    {
+      name_prefix = "blog-"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+      }
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
+    Type        = "Terraform"
+  }
+}
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
-  name = "blog_new"
+  name    = "blog_new"
 
   vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
