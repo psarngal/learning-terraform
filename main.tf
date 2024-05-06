a4f490aec977d16856a2ed5d3a9b6439ead09010
Pawan Sarngal <pawansarngal@gmail.com>
2024-05-06 17:04:20 +0530

    added SG Again

--- main.tf
+++ main.tf
@@ -104,41 +104,38 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-# resource "aws_security_group" "blog" {
-#   name = "blog"
-#   tags = {
-#     Terraform = "true"
-#   }
-#   vpc_id = data.aws_vpc.default.id
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
 
-# }
+}
 
-# resource "aws_security_group_rule" "blog_http_in" {
-#   security_group_id = aws_security_group.blog.id
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+resource "aws_security_group_rule" "blog_http_in" {
+  security_group_id = aws_security_group.blog.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "blog_https_in" {
-#   security_group_id = aws_security_group.blog.id
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
+resource "aws_security_group_rule" "blog_https_in" {
+  security_group_id = aws_security_group.blog.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-# resource "aws_security_group_rule" "blog_everything_out" {
-#   security_group_id = aws_security_group.blog.id
-#   type              = "egress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-# }
-
-
-
+resource "aws_security_group_rule" "blog_everything_out" {
+  security_group_id = aws_security_group.blog.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
